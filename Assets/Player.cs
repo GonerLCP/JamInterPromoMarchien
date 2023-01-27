e595f9280bbd16718ba789cb2fed77d8488f6946
Emilien Marchal <emilien.marchal@etu.univ-amu.fr>
2023-01-27 19:32:14 +0100

    PostProcessing Fonctionnel + Barre de vie

---
+++ Assets/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 5;
+    public int currentHealth;
+    public bool updateldvalue;
+    public float transparency = 0f;
+
+    public Healthbar healthbar;
+    public PostProcessingManager ppmanager;
+    public Image effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = 0;
+        healthbar.setMaxHealth(maxHealth);
+        effect.color = new Color(1f,0.5f, 0.9f, transparency);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            if(currentHealth < maxHealth)
+            {
+                Ilaprisdeladrogue(1);
+                updateldvalue = true;
+                transparency += 0.05f;
+            }
+            effect.color = new Color(1f, 0.5f, 0.9f, transparency);
+
+        }
+
+        void Ilaprisdeladrogue(int cocadelamama)
+        {
+            currentHealth += cocadelamama;
+            healthbar.setHealth(currentHealth);
+
+            float value = (float)currentHealth;
+            ppmanager.caIntensity(value);
+            ppmanager.blIntensity(value);
+        }
+    }
+}
